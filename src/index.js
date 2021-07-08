644e5b157c1adfafdb10bc49163b6429ea97cfe7
chrismann809 <chrismann809@gmail.com>
2021-07-08 09:21:43 -0400

    added powerups, bugfixes, title

--- src/index.js
+++ src/index.js
@@ -1,89 +1,282 @@
 function load() {
-    canvas = document.getElementById('canvas');
-    if (canvas.getContext){
-      ctx = canvas.getContext('2d');
-      gridSize = 20;
-      start();
-    } else {
-      alert("We're sorry, but your browser does not support the canvas tag. Please use any web browser other than Internet Explorer.");
-    }
+  canvas = document.getElementById('canvas');
+  if (canvas.getContext){
+    ctx = canvas.getContext('2d');
+    this.gridSize = 20;
+    start();
+  } else {
+    alert("Sorry, but Internet Expolorer does not support the canvas tag. Please use another web browser.");
+  }
 }
 
-function start() {
-    this.currentPosition = {'x': 100, 'y': 100};
-    drawSnake();
-    direction = 'right'
-    setInterval(moveSnake, 100)
+function start(){
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+  this.currentPosition = {'x':100, 'y':100};
+  this.count = 0;
+  this.powerUp = 0;
+  this.powerUpCount = 0;
+  this.invinsibleArray = [];
+  snakeBody = [];
+  snakeLength = 3;
+  startGame = false;
+  updateScore();
+  makeFoodItem();
+  drawSnake();
+  direction = 'right';
+  started = false;
+  // play();  
+}
+
+function restart(){
+  pause();
+  start();
+}
+
+function pause(){
+  clearInterval(interval);
+}
+
+function play(){
+  interval = setInterval(moveSnake,100);
+  started = true;
 }
 
 function drawSnake() {
-    ctx.fillStyle = "rgb(0, 200, 0)";
-    ctx.fillRect(currentPosition['x'], currentPosition['y'], gridSize, gridSize);
+  if (snakeBody.some(hasEatenItself) && this.powerUp !== 2) {
+    gameOver();
+    return false;
+  } else if (snakeBody.some(hasEatenItself) && this.powerUp === 2) {
+    this.invinsibleArray.push([currentPosition['x'], currentPosition['y']].toString());
+  }
+  snakeBody.push([currentPosition['x'], currentPosition['y']]);
+  if (this.powerUp === 0) {
+    ctx.fillStyle = "rgb(0,200,0)";
+  } else if (this.powerUp === 1) {
+    ctx.fillStyle = "rgb(255,0,129)";
+  } else if (this.powerUp === 2) {
+    ctx.fillStyle = "rgb(255,165,0)";
+  } else if (this.powerUp === 3) {
+    ctx.fillStyle = "rgb(51,255,255)";
+  } else {
+    ctx.fillStyle = "rgb(0,102,0)";
+  }
+  ctx.fillRect(currentPosition['x'], currentPosition['y'], gridSize - 1, gridSize - 1);
+  if (snakeBody.length > snakeLength) {
+    itemToRemove = snakeBody.shift();
+    var index = this.invinsibleArray.indexOf(itemToRemove.toString());
+    if (index !== -1) {
+      console.log(this.invinsibleArray);
+      console.log(itemToRemove);
+      console.log("test");
+    }
+    (index > -1) ? this.invinsibleArray.splice(index, 1) : ctx.clearRect(itemToRemove[0] - 0.1, itemToRemove[1] - 0.1, gridSize, gridSize);
+  }
+  if (this.count === 100) {
+    spawnPowerUp();
+  }  
+  if (currentPosition['x'] == suggestedPoint[0] && currentPosition['y'] == suggestedPoint[1]) {
+    makeFoodItem();
+    snakeLength += 1;
+    this.powerUp === 4 ? snakeLength += 1 : null;
+    updateScore();
+  }
+  if (this.count > 100 && currentPosition['x'] == powerUpPoint[0] && currentPosition['y'] == powerUpPoint[1]) {
+    this.powerUp = (Math.floor(Math.random() * 4) + 1);
+    powerUpPoint = [-1,-1];
+    console.log(this.powerUp);
+  }
+  if (this.powerUp > 0) {
+    pU = this.powerUp;
+    if (pU === 1 && this.powerUpCount === 0) {
+      clearInterval(interval);
+      interval = setInterval(moveSnake, 70);
+    } else if (pU === 2 && this.powerUpCount === 0) {
+      this.invinsibleArray = [];
+    } else if (pU === 3 && this.powerUpCount === 0) {
+      clearInterval(interval);
+      interval = setInterval(moveSnake, 130)
+    } else {
+      null;
+    }
+    if (this.powerUpCount >= 225) {
+      clearInterval(interval)
+      interval = setInterval(moveSnake, 100);
+      this.powerUp = 0;
+      this.powerUpCount = 0;
+      this.count = 0;
+    } else {
+      this.powerUpCount += 1;
+    }
+  }
 }
 
-function moveSnake(){
-    switch(direction){
-      case 'up':
-        currentPosition['y'] = currentPosition['y'] - gridSize;
-        drawSnake();
-        break;
-  
-      case 'down':
-        currentPosition['y'] = currentPosition['y'] + gridSize;
-        drawSnake();
-        break;
-  
-      case 'left':
-        currentPosition['x'] = currentPosition['x'] - gridSize;
-        drawSnake();
-        break;
-  
-      case 'right':
-        currentPosition['x'] = currentPosition['x'] + gridSize;
-        drawSnake();
-        break;
-    }
+function includesItemToRemove(element, index, array) {
+  return element[0] === itemToRemove[0] && element[1] === itemToRemove[1];
+}
+
+function leftPosition(){
+ return currentPosition['x'] - gridSize;  
+}
+
+function rightPosition(){
+  return currentPosition['x'] + gridSize;
+}
+
+function upPosition(){
+  return currentPosition['y'] - gridSize;  
+}
+
+function downPosition(){
+  return currentPosition['y'] + gridSize;
+}
+
+function moveUp(){
+  if (upPosition() >= 0) {
+    executeMove('up', 'y', upPosition());
+  } else {
+    gameOver();
+  }
+}
+
+function moveDown(){
+  if (downPosition() < canvas.height) {
+    executeMove('down', 'y', downPosition());    
+  } else {
+    gameOver();
+  }
+}
+
+function moveLeft(){
+  if (leftPosition() >= 0) {
+    executeMove('left', 'x', leftPosition());
+  } else {
+    gameOver();
+  }
+}
+
+function moveRight(){
+  if (rightPosition() < canvas.width) {
+    executeMove('right', 'x', rightPosition());
+  } else {
+    gameOver();
+  }
+}
+
+function executeMove(dirValue, axisType, axisValue) {
+  direction = dirValue;
+  currentPosition[axisType] = axisValue;
+  this.count += 1;
+  drawSnake();
+}
+
+function makeFoodItem(){
+  suggestedPoint = [Math.floor(Math.random()*(canvas.width/gridSize))*gridSize, Math.floor(Math.random()*(canvas.height/gridSize))*gridSize];
+  if (snakeBody.some(hasFoodPoint)) {
+    makeFoodItem();
+  } else {
+    ctx.fillStyle = "rgb(255,51,51)";
+    ctx.fillRect(suggestedPoint[0], suggestedPoint[1], gridSize - 2, gridSize - 2);
+  };
+}
+
+function spawnPowerUp(){
+  powerUpPoint = [Math.floor(Math.random()*(canvas.width/gridSize))*gridSize, Math.floor(Math.random()*(canvas.height/gridSize))*gridSize];
+  if (snakeBody.some(hasPowerUpPoint)) {
+    spawnPowerUp();
+  } else {
+    ctx.fillStyle = "rgb(192,192,192)";
+    ctx.fillRect(powerUpPoint[0], powerUpPoint[1], gridSize - 3, gridSize - 3);
+  };
+}
+
+function hasFoodPoint(element, index, array) {
+  return (element[0] == suggestedPoint[0] && element[1] == suggestedPoint[1]);
+}
+
+function hasPowerUpPoint(element, index, array) {
+  return (element[0] == powerUpPoint[0] && element[1] == powerUpPoint[1]);
+}
+
+function hasEatenItself(element, index, array) {
+  return (element[0] == currentPosition['x'] && element[1] == currentPosition['y']);  
+}
+
+function gameOver(){
+  var score = (snakeLength - 3)*100;
+  pause();
+  alert("Game Over. Your score was "+ score);
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+  started = false;
+  restart();
+}
+
+function updateScore(){
+  var score = (snakeLength - 3)*25;
+  document.getElementById('score').innerText = score;
 }
 
 document.onkeydown = function(event) {
-  var key;
-
+  var keyCode; 
   if(event == null)
   {
-    key = window.event.keyCode;
+    keyCode = window.event.keyCode; 
   }
-  else
+  else 
   {
-    key = event.keyCode;
+    keyCode = event.keyCode; 
   }
-
-  switch(key) //take in key code and move in according direction
-    {
+ 
+  switch(keyCode)
+  {
+    case 32:
+      if (started === false) {
+        play();
+      }
     case 37:
-        direction = 'left';
-        currentPosition['x'] = currentPosition['x'] - gridSize;
-        ctx.fillRect(currentPosition['x'], currentPosition['y'], gridSize, gridSize);
-        break;
-
+      if (direction != "right" && started != false){
+        moveLeft();
+      }
+      break;
+     
     case 38:
-        direction = 'up';
-        currentPosition['y'] = currentPosition['y'] - gridSize;
-        ctx.fillRect(currentPosition['x'], currentPosition['y'], gridSize, gridSize);
-        break;
-
+      if (direction != "down" && started != false){
+        moveUp();
+      }
+      break; 
+      
     case 39:
-        direction = 'right';
-        currentPosition['x'] = currentPosition['x'] + gridSize;
-        ctx.fillRect(currentPosition['x'], currentPosition['y'], gridSize, gridSize);
-        break;
-
+      if (direction != "left" && started != false){
+        moveRight();
+      }
+      break; 
+    
     case 40:
-        direction = 'down'
-        currentPosition['y'] = currentPosition['y'] + gridSize;
-        ctx.fillRect(currentPosition['x'], currentPosition['y'], gridSize, gridSize);
-        break;
+      if (direction != "up" && started != false){
+        moveDown();
+      }
+      break; 
+    
+    default: 
+      break; 
+  } 
+}
 
-    default:
-        break;
-    }
+function moveSnake(){
+  switch(direction){
+    case 'up':
+      moveUp();
+      break;
+
+    case 'down':
+      moveDown();
+      break;
+      
+    case 'left':
+      moveLeft();
+      break;
+
+    case 'right':
+      moveRight();
+      break;
+  }
 }
